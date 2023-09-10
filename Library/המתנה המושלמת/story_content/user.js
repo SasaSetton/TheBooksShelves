d8146769002cfa44698387c69d8354b11b9691ce
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-09-10 21:48:34 +0300

    Add files via upload

---
+++ Library/המתנה המושלמת/story_content/user.js
@@ -0,0 +1,87 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ex9yE3HieO":
+        Script1();
+        break;
+      case "6ESAFocEjvx":
+        Script2();
+        break;
+      case "5sG2uiSxRZp":
+        Script3();
+        break;
+      case "65sPMsnbGse":
+        Script4();
+        break;
+      case "6Qb4mohc8Ab":
+        Script5();
+        break;
+      case "6Ed0DhMkjuJ":
+        Script6();
+        break;
+      case "6dkM0gBCC2n":
+        Script7();
+        break;
+      case "5fYIR7KqVRF":
+        Script8();
+        break;
+      case "6IpVHB3hf1X":
+        Script9();
+        break;
+      case "6BQnaGJExmR":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
+function Script5()
+{
+  window.print();
+}
+
+function Script6()
+{
+  window.print();
+}
+
+function Script7()
+{
+  window.print();
+}
+
+function Script8()
+{
+  window.print();
+}
+
+function Script9()
+{
+  window.print();
+}
+
+function Script10()
+{
+  window.print();
+}
+
