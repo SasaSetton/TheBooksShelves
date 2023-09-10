415de41c8385ee71d07d7428db5b2e7f2f9cdd13
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-09-10 21:57:14 +0300

    Add files via upload

---
+++ Library/המפלצות של נועם/story_content/user.js
@@ -0,0 +1,63 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6NPTr5CzMUg":
+        Script1();
+        break;
+      case "6hmED1HGolK":
+        Script2();
+        break;
+      case "6b4YViZrIqc":
+        Script3();
+        break;
+      case "5m5UxoBNAnm":
+        Script4();
+        break;
+      case "5enXaLccb7H":
+        Script5();
+        break;
+      case "5v8QScLkIeI":
+        Script6();
+        break;
+      case "6TdDKKhZZ0G":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
+function Script5()
+{
+  window.print();
+}
+
+function Script6()
+{
+  window.print();
+}
+
+function Script7()
+{
+  window.print();
+}
+
