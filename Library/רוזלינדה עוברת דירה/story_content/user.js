f57b31fb03b2df4f21a7c0b4c64730d686f42252
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-09-20 16:33:11 +0300

    Add files via upload

---
+++ Library/רוזלינדה עוברת דירה/story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Y4disdFOgH":
+        Script1();
+        break;
+      case "5e82rlkToAf":
+        Script2();
+        break;
+      case "5ZIopz1aySz":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
