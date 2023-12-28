c59fe74d36ce40e4db9b0648c615e8878be7772c
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2023-12-28 13:05:11 +0200

    Add files via upload

--- Library/קלידוסקופ/story_content/user.js
+++ Library/קלידוסקופ/story_content/user.js
@@ -2,6 +2,129 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5lmA8Q85tJx":
+        Script1();
+        break;
+      case "64TC6ceeA4p":
+        Script2();
+        break;
+      case "5fUQ4fCdU22":
+        Script3();
+        break;
+      case "6R2BpIjm0Fj":
+        Script4();
+        break;
+      case "5abKsa2b1Z5":
+        Script5();
+        break;
+      case "6Je7DQujAbP":
+        Script6();
+        break;
+      case "6MGy4uHH3Ip":
+        Script7();
+        break;
+      case "6BQCWGuyaep":
+        Script8();
+        break;
+      case "6Qr8H27v1JQ":
+        Script9();
+        break;
+      case "6DDefvv5sd7":
+        Script10();
+        break;
+      case "6PmxgEuRfz7":
+        Script11();
+        break;
   }
+}
+
+function Script1()
+{
+  if(void 0===window.stencilsheets){t="https://cluelabs.com/stencil/display/widget-sheets-init-v2",s={authtool:"sl",chart:"MTQ5M3w0NDQ4fDA4ZmFlMGMzM2ZmMzVkNGE5MGNhN2FiNzFiNjlmNDE5"},a=Object.keys(s).map(e=>encodeURIComponent(e)+"="+encodeURIComponent(s[e])).join("&");(xhttp=new XMLHttpRequest).onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],s=document.createElement("script");t.appendChild(s),s.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("POST",t,!0),xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),xhttp.send(a)}
+        
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+SheetsSet('כינוי', player.GetVar('name'));
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+SheetsSet('שם בית החולים', player.GetVar('HospitalName'));
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+SheetsSet('שם המדף', player.GetVar('shelf'));
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+SheetsSet('שם הספר', player.GetVar('BookName'));
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+SheetsSet('משימת כתיבה', 
+			player.GetVar('פנייהלנמען') +', '+ player.GetVar('משפטפתיחה') +' '+ player.GetVar('גוףהמכתב') +' '+ player.GetVar('משפטסיכום') +' '+ player.GetVar('חתימתהמוען')
+		);
+
+
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+SheetsSet('שאלה 2', 
+			'שאלה: מהן התכונות הבולטות של אווה?, תשובה: '+ player.GetVar('TextEntry16')
+		);
+
+
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+SheetsSet('שאלה 3', 
+			'שאלה: כיצד מצטיירים מהסיפור החיים בגטו?, תשובה: '+ player.GetVar('TextEntry11')
+		);
+
+
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+SheetsSet('שאלה 4', 
+			'שאלה: מה דעתך על דמותה של ברוניסלבה?, תשובה: '+ player.GetVar('TextEntry12')
+		);
+
+
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+SheetsSet('שאלה 5', 
+			'שאלה: מה חשבת על הסיפור?, תשובה: '+ player.GetVar('TextEntry13')
+		);
+
+
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+SheetsSet('שאלה 1', 
+			'שאלה: מדוע לדעתך נכתב הסיפור?, תשובה: '+ player.GetVar('TextEntry15')
+		);
+
+
 }
 
