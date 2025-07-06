70f91397172b51eab5a06284915a42a88363fe9a
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2025-07-06 22:12:13 +0300

    Add files via upload

---
+++ Library/נוני ונוני יותר/story_content/triggers.js
@@ -0,0 +1,34 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5fcApJZVPnK":
+        Script1();
+        break;
+      case "5YmiTRR3CpS":
+        Script2();
+        break;
+      case "5YXP3VVhLjy":
+        Script3();
+        break;
+      case "6IkotVFPvZz":
+        Script4();
+        break;
+      case "60IaUEOvOe5":
+        Script5();
+        break;
+      case "5k5ZhzTXPrM":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
--- Library/נוני ונוני יותר/story_content/user.js
+++ Library/נוני ונוני יותר/story_content/user.js
@@ -1,59 +1,42 @@
-function ExecuteScript(strId)
+window.InitUserScripts = function()
 {
-  switch (strId)
-  {
-      case "5V4yYlfIYMy":
-        Script1();
-        break;
-      case "5qhC3i6F1nM":
-        Script2();
-        break;
-      case "6HffXh2h9uR":
-        Script3();
-        break;
-      case "6315lnN0xg6":
-        Script4();
-        break;
-      case "6YldOhq5IpW":
-        Script5();
-        break;
-      case "5VMgNkn0p6v":
-        Script6();
-        break;
-  }
-}
-
-function Script1()
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
 {
   if(void 0===window.stencilsheets){t="https://cluelabs.com/stencil/display/widget-sheets-init-v2",s={authtool:"sl",chart:"MTQ5M3w0NDQ4fDA4ZmFlMGMzM2ZmMzVkNGE5MGNhN2FiNzFiNjlmNDE5"},a=Object.keys(s).map(e=>encodeURIComponent(e)+"="+encodeURIComponent(s[e])).join("&");(xhttp=new XMLHttpRequest).onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],s=document.createElement("script");t.appendChild(s),s.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("POST",t,!0),xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),xhttp.send(a)}
         
 }
 
-function Script2()
+window.Script2 = function()
 {
   var player = GetPlayer();
 SheetsSet('כינוי', player.GetVar('name'));
 }
 
-function Script3()
+window.Script3 = function()
 {
   var player = GetPlayer();
 SheetsSet('שם בית החולים', player.GetVar('HospitalName'));
 }
 
-function Script4()
+window.Script4 = function()
 {
   var player = GetPlayer();
 SheetsSet('שם המדף', player.GetVar('shelf'));
 }
 
-function Script5()
+window.Script5 = function()
 {
   var player = GetPlayer();
 SheetsSet('שם הספר', player.GetVar('BookName'));
 }
 
-function Script6()
+window.Script6 = function()
 {
   var player = GetPlayer();
 SheetsSet('שאלה 4', 
@@ -63,3 +46,4 @@ SheetsSet('שאלה 4',
 
 }
 
+};
