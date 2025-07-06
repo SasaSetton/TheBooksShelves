33e3ef775a7d2c32b7b5a96d70a34a76c1362205
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2025-07-06 21:51:19 +0300

    Add files via upload

---
+++ Library/שניים שבלוליים/story_content/triggers.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5oO6uu7Svhz":
+        Script1();
+        break;
+      case "662sdLq02HU":
+        Script2();
+        break;
+      case "6RjNF6YCkID":
+        Script3();
+        break;
+      case "6MqEpXMc7LK":
+        Script4();
+        break;
+      case "6Y1jXKBxOLn":
+        Script5();
+        break;
+      case "6juJWWqYaTD":
+        Script6();
+        break;
+      case "5eE2qwKwXrI":
+        Script7();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
