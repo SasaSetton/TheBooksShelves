8805617f764786add41d4a8ee8a048cd6c040bf4
SasaSetton <118336454+SasaSetton@users.noreply.github.com>
2025-07-20 21:41:03 +0300

    Add files via upload

--- Library/נוני ונוני יותר/story_content/user.js
+++ Library/נוני ונוני יותר/story_content/user.js
@@ -1,42 +1,59 @@
-window.InitUserScripts = function()
+function ExecuteScript(strId)
 {
-var player = GetPlayer();
-var object = player.object;
-var once = player.once;
-var addToTimeline = player.addToTimeline;
-var setVar = player.SetVar;
-var getVar = player.GetVar;
-window.Script1 = function()
+  switch (strId)
+  {
+      case "6W1MmiZ1gHq":
+        Script1();
+        break;
+      case "5yFt6ETgazz":
+        Script2();
+        break;
+      case "5jlbV2jNLgF":
+        Script3();
+        break;
+      case "5nly03RNRRf":
+        Script4();
+        break;
+      case "5g0Y4AD0gfH":
+        Script5();
+        break;
+      case "5pPKlHAuV1X":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
 {
   if(void 0===window.stencilsheets){t="https://cluelabs.com/stencil/display/widget-sheets-init-v2",s={authtool:"sl",chart:"MTQ5M3w0NDQ4fDA4ZmFlMGMzM2ZmMzVkNGE5MGNhN2FiNzFiNjlmNDE5"},a=Object.keys(s).map(e=>encodeURIComponent(e)+"="+encodeURIComponent(s[e])).join("&");(xhttp=new XMLHttpRequest).onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],s=document.createElement("script");t.appendChild(s),s.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("POST",t,!0),xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),xhttp.send(a)}
         
 }
 
-window.Script2 = function()
+function Script2()
 {
   var player = GetPlayer();
 SheetsSet('כינוי', player.GetVar('name'));
 }
 
-window.Script3 = function()
+function Script3()
 {
   var player = GetPlayer();
 SheetsSet('שם בית החולים', player.GetVar('HospitalName'));
 }
 
-window.Script4 = function()
+function Script4()
 {
   var player = GetPlayer();
 SheetsSet('שם המדף', player.GetVar('shelf'));
 }
 
-window.Script5 = function()
+function Script5()
 {
   var player = GetPlayer();
 SheetsSet('שם הספר', player.GetVar('BookName'));
 }
 
-window.Script6 = function()
+function Script6()
 {
   var player = GetPlayer();
 SheetsSet('שאלה 4', 
@@ -46,4 +63,3 @@ SheetsSet('שאלה 4',
 
 }
 
-};
